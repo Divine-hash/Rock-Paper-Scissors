b817315e9bd8ba2bc0bae8684c1f232a8382aa8c
ifeanyi divine onuoha <ifeanyidivineonuoha@gmail.com>
2022-03-31 23:32:26 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ function computerPlay() {
   return arr[index];
 }
 
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (computerSelection == 'paper' && playerSelection == 'rock') {
+    return 'You lose, Paper beats rock!';
+  }
+
+  if (computerSelection == 'rock' && playerSelection == 'scissors') {
+    return 'You lose, Rock beats scissors';
+  }
+
+  if (computerSelection == 'scissors' && playerSelection == 'paper') {
+    return 'You lose, Scissors beats paper';
+  }
+  
+  if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return 'You win, Paper beats rock';
+  }
+
+  if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return 'You win, Rock beats scissors';
+  }
+
+  if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    return 'You win, Scissors beats paper';
+  }
+
+  if (computerSelection == playerSelection) {
+    return 'It\'s a tie';
+  }
+}
+
+let playerSelection = 'Rock';
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
