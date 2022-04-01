78cdd52ae20888b50b3b39fd0adc64020d7fd5d5
ifeanyi divine onuoha <ifeanyidivineonuoha@gmail.com>
2022-04-01 05:53:04 +0100

    refactor playRound function

--- script.js
+++ script.js
@@ -6,35 +6,24 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
-  if (computerSelection == 'paper' && playerSelection == 'rock') {
-    return 'You lose, Paper beats rock!';
-  }
-
-  if (computerSelection == 'rock' && playerSelection == 'scissors') {
-    return 'You lose, Rock beats scissors';
-  }
-
-  if (computerSelection == 'scissors' && playerSelection == 'paper') {
-    return 'You lose, Scissors beats paper';
-  }
-  
-  if (playerSelection == 'paper' && computerSelection == 'rock') {
-    return 'You win, Paper beats rock';
-  }
-
-  if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    return 'You win, Rock beats scissors';
-  }
-
-  if (playerSelection == 'scissors' && computerSelection == 'paper') {
-    return 'You win, Scissors beats paper';
-  }
-
-  if (computerSelection == playerSelection) {
-    return 'It\'s a tie';
-  }
+  let result = 
+    playerSelection == computerSelection
+      ? 'Tie game'
+      : playerSelection == 'rock' && computerSelection == 'scissors'
+      ? 'You win, Rock beats scissors'
+      : playerSelection == 'paper' && computerSelection == 'rock'
+      ? 'You win, Paper beats rock'
+      : playerSelection == 'scissors' && computerSelection == 'paper'
+      ? 'You win, scissors beats paper'
+      : computerSelection == 'rock' && playerSelection == 'scissors'
+      ? 'You lose, Rock beats scissors'
+      : computerSelection == 'paper' && playerSelection == 'rock'
+      ? 'You lose, Paper beats rock'
+      : computerSelection == 'scissors' && playerSelection == 'paper'
+      ? 'You lose, scissors beats paper' : null; 
+  return result;
 }
 
-let playerSelection = 'Rock';
+let playerSelection = 'Scissors';
 let computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
