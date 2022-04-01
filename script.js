9dce40a7be72c7813fcc0e728d7108e2118a6cdc
ifeanyi divine onuoha <ifeanyidivineonuoha@gmail.com>
2022-04-01 19:27:40 +0100

    Add function to display final winner

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function computerPlay() {
   return arr[index];
 }
 
+// The playRound function returns undefined when neither rock, 
+// paper or scissors is the input.
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
@@ -26,23 +28,20 @@ function playRound(playerSelection, computerSelection) {
         : computerSelection == 'scissors' && playerSelection == 'paper'
         ? 'You lose, scissors beats paper' : null; 
     return result;
-
-    }
-
+  }
 }
 
 function game() {
   let play = confirm('Play Round 1 of 5?');
   let playerPoints = 0;
   let computerPoints = 0;
-  let winner;
 
   if (play) {
     for (let i = 1; i <= 5; i++) {
       let round = i;
       let playerSelection = prompt('Choose Rock, Paper, or Scissors', '');
       let computerSelection = computerPlay();
-      winner = playRound(playerSelection, computerSelection); // Returns the winner for a single round else undefined
+      let winner = playRound(playerSelection, computerSelection); // Returns the winner for a single round else undefined
 
       //output the winner if winner is not undefined
       if (winner) {
@@ -50,38 +49,38 @@ function game() {
         if (winner.includes('lose')) computerPoints += 1;
         
         console.log(`Round ${i}: ${winner}`);
-        if (round == 5) return;
+        if (round == 5) break;
         play = confirm(`Play Round ${++round} of 5`);
+
         if (!play) {
-          finalWinner();
+          finalWinner(playerPoints, computerPoints); 
           return;
         };
+
       } else {
-        //decrement i
-        --i;
+        --i; // decrement i and run that round again if "winner" is
       }
     }
-
-    function finalWinner() {
+     
+    function finalWinner(playerPoints, computePoints) {
       if (playerPoints > computerPoints) {
-        console.log(`Your Score: ${playerPoints} 
-        Computer Score ${computerPoints}`);
-        console.log('You Win');
+        console.log(`Your Score: ${playerPoints} Computer Score: ${computerPoints}`);
+        console.log('You Win!');
         return;
       }
       
       if (playerPoints < computePoints) {
-        console.log(`Your Score: ${playerPoints} Computer Score ${computerPoints}`);
-        console.log('You loose');
+        console.log(`Your Score: ${playerPoints} Computer Score: ${computerPoints}`);
+        console.log('You lose!');
         return;
       }
 
-      
-      console.log(`Your Score: ${playerPoints} 
-      Computer Score ${computerPoints}`);
-      console.log('Tie game');
+      console.log(`Your Score: ${playerPoints} Computer Score: ${computerPoints}`);
+      console.log('Tie game!');
     }
-    
+
+    finalWinner(playerPoints, computerPoints);
+
   }
 }
 
